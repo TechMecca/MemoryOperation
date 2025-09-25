082df509ecd5d009e15c984a354cca758657a92f
TechMecca <jaywalls632@gmail.com>
2025-09-25 15:55:47 -0700

    Testfix2

--- MemoryOperation/Include/MemoryOperator.h
+++ MemoryOperation/Include/MemoryOperator.h
@@ -10,21 +10,11 @@ class MemoryOperator
 {
 public:
     static Patch* CreatePatch(const std::string& name, uintptr_t address, const std::vector<byte>& bytes);
+
+    // Only need this non-template version
     static WinDetour* CreateDetour(const std::string& name, uintptr_t target_addr, uintptr_t detour_addr);
 
-    template<typename T>
-    static WinDetour* CreateDetour(const std::string& name, uintptr_t target_addr, T&& detour_func)
-    {
-        // Store the detour function in a persistent object
-        static auto persistent_func = std::forward<T>(detour_func);
-        uintptr_t detour_addr = reinterpret_cast<uintptr_t>(+[]() {
-            persistent_func();
-            });
-
-        return CreateDetour(name, target_addr, detour_addr);
-    }
-
-    static Patch*     FindPatch(const std::string& name);
+    static Patch* FindPatch(const std::string& name);
     static WinDetour* FindDetour(const std::string& name);
 
 private:
