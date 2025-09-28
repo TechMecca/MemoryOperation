9ffcc715b6bcd234a6fad1d6e39f47e3e9938a86
TechMecca <jaywalls632@gmail.com>
2025-09-27 23:39:07 -0700

    Added Traceback From Oxkate

---
+++ MemoryOperation/Include/Traceback.h
@@ -0,0 +1,52 @@
+#pragma once
+#include "Windows.h"
+#include <cstdint>
+#include <stdio.h>
+#include <array>
+#include <vector>
+
+
+class Traceback {
+public:
+    struct FrameInfo {
+        uintptr_t CallAddress{};
+        USHORT    StackIndex{};
+        void Dump() const
+        {
+            char msg[1024];
+            _snprintf_s(msg, sizeof(msg), _TRUNCATE,
+                "[FrameInfo] %-2u) - " "0x%08x",
+                StackIndex, CallAddress);
+
+        }
+
+    };
+
+    struct TraceInfo {
+        USHORT StackSize{};
+        std::array<void*, 62> Stack{};
+        std::vector<FrameInfo> Frames{};
+        void Dump() const {
+            for (FrameInfo frame : Frames) {
+                frame.Dump();
+            }
+        }
+    };
+
+    static TraceInfo Capture(const USHORT skip = 1, USHORT maxFrames = 62) {
+        if (maxFrames > 62) maxFrames = 62;
+
+        auto traceback = TraceInfo{};
+
+        traceback.StackSize = RtlCaptureStackBackTrace(skip, maxFrames, traceback.Stack.data(), nullptr);
+        traceback.Frames.reserve(traceback.StackSize);
+
+        for (USHORT i = 0; i < traceback.StackSize; ++i) {
+            traceback.Frames.push_back(FrameInfo{
+                reinterpret_cast<uintptr_t>(traceback.Stack[i]),
+                i
+                });
+        }
+        return traceback;
+    }
+};
